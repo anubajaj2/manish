0b209290dd4e67e89f7b616ef31fc9a5db23e4fd
anubajaj2 <anurag.bajaj02@gmail.com>
2020-07-05 23:41:28 +0530

    images in lightbox

--- client/controller/productSearch.controller.js
+++ client/controller/productSearch.controller.js
@@ -29,6 +29,35 @@ sap.ui.define([
 			//alert("yo");
 			debugger;
 		},
+		onImageOut: function(oEvent){
+			oEvent.getSource().setSrc("https://5.imimg.com/data5/RU/WR/MY-8087605/kundan-meena-set-500x500.jpg");
+		},
+		onImageIn: function(oEvent){
+			oEvent.getSource().setSrc("https://img5.cfcdn.club/5e/cb/5ef37886b3ad099ddb939520191ec4cb_350x350.jpg");
+		},
+		onImageOpen: function(){
+			
+		},
+		onRefresh: function () {
+			// trigger search again and hide pullToRefresh when data ready
+			var oProductList = this.byId("gridList");
+			var bShowSearchResults = false;
+			var oBinding = oProductList.getBinding("items");
+			var fnHandler = function () {
+				//this.byId("pullToRefresh").hide();
+				oBinding.detachDataReceived(fnHandler);
+			}.bind(this);
+			oBinding.attachDataReceived(fnHandler);
+
+			if (oBinding) {
+				if (bShowSearchResults) {
+					var oFilter = new Filter("Name", FilterOperator.Contains, oSearchField.getValue());
+					oBinding.filter([oFilter]);
+				} else {
+					oBinding.filter([]);
+				}
+			}
+		},
 		/**
 		 * Always navigates back to category overview
 		 * @override
