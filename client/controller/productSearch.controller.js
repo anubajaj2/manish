0eec7a3d278422e10afa472b3438b88935ad7624
anubajaj2 <anurag.bajaj02@gmail.com>
2020-07-06 17:26:47 +0530

    Images and details popover

--- client/controller/productSearch.controller.js
+++ client/controller/productSearch.controller.js
@@ -6,7 +6,11 @@ sap.ui.define([
 	"sap/ui/model/FilterOperator",
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/Fragment"
+	"sap/ui/core/Fragment",
+	"sap/m/Image",
+	"sap/m/Dialog",
+	"sap/m/Carousel",
+	"sap/m/Button"
 ], function (
 	BaseController,
 	formatter,
@@ -15,7 +19,8 @@ sap.ui.define([
 	FilterOperator,
 	MessageToast,
 	JSONModel,
-	Fragment) {
+	Fragment,
+	Image, Dialog, Carousel, Button) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.cart.controller.productSearch", {
@@ -36,7 +41,60 @@ sap.ui.define([
 			oEvent.getSource().setSrc("https://img5.cfcdn.club/5e/cb/5ef37886b3ad099ddb939520191ec4cb_350x350.jpg");
 		},
 		onImageOpen: function(){
-			
+			if (!this.pressDialog) {
+				this.pressDialog = new Dialog({
+					title: "Available Images",
+					stretch: true,
+					content: new Carousel({
+						pages:[
+								new Image({src : "https://5.imimg.com/data5/RU/WR/MY-8087605/kundan-meena-set-500x500.jpg"}),
+								new Image({src : "https://img5.cfcdn.club/5e/cb/5ef37886b3ad099ddb939520191ec4cb_350x350.jpg"})
+						]
+					}),
+					beginButton: new Button({
+						type: "Emphasized",
+						text: "Close",
+						press: function () {
+							this.pressDialog.close();
+						}.bind(this)
+					})
+				});
+
+				//to get access to the global model
+				this.getView().addDependent(this.pressDialog);
+			}
+
+			this.pressDialog.open();
+		},
+		onSelectProduct: function(oEvent){
+			//popover of product details
+			var oButton = oEvent.getSource();
+			//get binding path of parent list item
+			var sPath = oButton.getParent().getBindingContext().getPath();
+
+			if (!this._oPopover) {
+				Fragment.load({
+					name: "sap.ui.demo.cart.fragments.productDetails",
+					controller: this
+				}).then(function(oPopover){
+					this._oPopover = oPopover;
+					this.getView().addDependent(this._oPopover);
+					this._oPopover.bindElement(sPath);
+					this._oPopover.openBy(oButton);
+				}.bind(this));
+			} else {
+				this._oPopover.bindElement(sPath);
+				this._oPopover.openBy(oButton);
+			}
+
+		},
+		handleCloseButton: function(){
+			this._oPopover.close();
+		},
+		onExit : function () {
+			if (this._oPopover) {
+				this._oPopover.destroy();
+			}
 		},
 		onRefresh: function () {
 			// trigger search again and hide pullToRefresh when data ready
