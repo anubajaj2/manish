e444f5c332768b72523677879360ffaef177fbf5
anubajaj2 <anurag.bajaj02@gmail.com>
2020-07-08 22:55:04 +0530

    user api

--- server/server.js
+++ server/server.js
@@ -36,6 +36,153 @@ app.start = function() {
 	});
 };
 
+app.post('/updateLastLogin',
+	function(req, res) {
+		if (!req.body.emailId) {
+			res.send('No Email Id');
+			return;
+		}
+		this.AppUser = app.models.AppUser;
+		var _this = this;
+		_this.AppUser.findOne({ where: { "EmailId": req.body.emailId } }).then(function (appUser) {
+				if (appUser) {
+						appUser.updateAttributes({
+							lastLogin: new Date()
+						});
+						res.send("Change Success");
+				}else{
+					res.send("User not found");
+				}
+		}).catch(function(err){
+			res.send("Error Occurred");
+		});
+
+	}
+);
+
+app.post('/changePassword',
+	function(req, res) {
+		if (!req.body.emailId) {
+			res.send('No Email Id');
+			return;
+		}
+		if (!req.body.newPassword) {
+			//Retailer, Admin, Maker
+			res.send('Password is empty');
+			return;
+		}
+		if (!req.body.Authorization) {
+			res.send('No Authorization');
+			return;
+		}
+
+		this.Token = app.models.AccessToken;
+		this.User = app.models.User;
+		this.Role = app.models.Role;
+		this.AppUser = app.models.AppUser;
+		this.RoleMapping = app.models.RoleMapping;
+		var _this = this;
+		this.Token.findById(req.body.Authorization).then(function(token){
+			var _this2 = _this;
+			_this2.userId = token.userId;
+			_this.User.findOne({ where: { email: req.body.emailId } }).then(function (user) {
+				if (user) {
+							user.updateAttributes({
+								password: req.body.newPassword
+							});
+							var _this3 = _this2;
+							_this2.TechnicalId = user.id;
+							_this2.AppUser.findOne({ where: { "EmailId": user.email } }).then(function (appUser) {
+									if (appUser) {
+											appUser.updateAttributes({
+												pwdChange: false,
+												ChangedBy: _this3.TechnicalId
+											});
+											res.send("Change Success");
+									}
+							});
+					}else{
+						res.send("User Not Found");
+					}
+			}).catch(function (err) {
+					res.send("You are not Authorized to perform this action");
+			});
+		}).catch(function(err){
+			res.send("You are not Authorized to perform this action");
+		});
+
+	}
+);
+
+app.post('/createNewUser',
+	function(req, res) {
+		if (!req.body.name) {
+			res.send('No user name sent');
+			return;
+		}
+		if (!req.body.emailId) {
+			res.send('No Email Id');
+			return;
+		}
+		if (!req.body.role) {
+			//Retailer, Admin, Maker
+			res.send('No Role');
+			return;
+		}
+		if (!req.body.Authorization) {
+			res.send('No Authorization');
+			return;
+		}
+
+		this.Token = app.models.AccessToken;
+		this.User = app.models.User;
+		this.Role = app.models.Role;
+		this.AppUser = app.models.AppUser;
+		this.RoleMapping = app.models.RoleMapping;
+		var _this = this;
+		this.Token.findById(req.body.Authorization).then(function(token){
+			var _this2 = _this;
+			_this2.userId = token.userId;
+			_this.User.findOne({ where: { email: req.body.emailId } }).then(function (user) {
+				if (!user) {
+							var _this3 = _this2;
+							_this2.User.create({ username: req.body.name, email: req.body.emailId, password: 'Welcome1' }).then(function (user) {
+									if (user) {
+										var _this4 = _this3;
+										_this3.TechnicalId = user.id;
+										_this3.AppUser.findOne({ where: { "EmailId": user.email } }).then(function (roleMapping) {
+											debugger;
+												if (!roleMapping) {
+														_this4.AppUser.create({
+																TechnicalId: _this4.TechnicalId,
+																EmailId: req.body.emailId,
+																UserName: req.body.name,
+																Role: req.body.role,
+																CreatedOn: new Date(),
+																CreatedBy: _this4.userId,
+																blocked: false,
+																pwdChange: true,
+																lastLogin: new Date()
+														}).then(function (roleMapping) {
+															res.send("yes created");
+														});
+												}
+										});
+									}
+							});
+					}else{
+						res.send("User Already Exist!!");
+					}
+			}).catch(function (err) {
+					res.send("You are not Authorized to perform this action");
+			});
+		}).catch(function(err){
+			res.send("You are not Authorized to perform this action");
+		});
+
+	}
+);
+
 app.post('/upload',
 	function(req, res) {
 		if (!req.files.myFileUpload) {
