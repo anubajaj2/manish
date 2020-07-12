5ffffd34717ea005d1f25c01dd2bafa88929adeb
neha <nehapatil217@gmail.com>
2020-07-12 18:37:30 +1000

    Group Permission View

--- client/controller/GroupPermission.controller.js
+++ client/controller/GroupPermission.controller.js
@@ -1,12 +1,45 @@
 sap.ui.define([
 	"sap/ui/demo/cart/controller/BaseController",
-	"sap/ui/core/UIComponent"
-], function(BaseController, UIComponent) {
+	"sap/ui/core/UIComponent",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, UIComponent, MessageToast, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.cart.controller.GroupPermission", {
+		//onInit: function () {
+			//this._router = UIComponent.getRouterFor(this);
+		//},
 		onInit: function () {
-			this._router = UIComponent.getRouterFor(this);
-		}
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("GroupPermission").attachMatched(this._onRouteMatched, this);
+
+		},
+		_onRouteMatched: function(oEvent) {
+				this.loadGroupPermissions();
+			},
+
+			loadGroupPermissions: function(){
+				debugger;
+					var that = this;
+					this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+								"/Groups", "GET", {}, {}, this)
+								.then(function(oData) {
+					that.getOwnerComponent().getModel("local").setProperty("/list",{
+					Groups:oData.results
+					});
+
+					})
+					.catch(function(oError) {
+							MessageToast.show("cannot fetch the data");
+					 });
+				},
+				onSelectItem: function(oEvent){
+					debugger;
+					//this.getModel("local").setProperty("/CurrentUser")
+          //this.getView().getId("Title1").setTitle("Group01")
+					this.getRouter().navTo("GroupPermission01");
+
+			}
 	});
 });
