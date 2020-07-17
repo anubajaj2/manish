27085773695b33edbe5e6d1072b3245a63688ea5
neha <nehapatil217@gmail.com>
2020-07-17 18:12:28 +1000

    Preferences View

--- client/controller/Preferences.controller.js
+++ client/controller/Preferences.controller.js
@@ -1,12 +1,36 @@
 sap.ui.define([
 	"sap/ui/demo/cart/controller/BaseController",
-	"sap/ui/core/UIComponent"
-], function(BaseController, UIComponent) {
+	"sap/ui/core/UIComponent",
+	"sap/f/LayoutType"
+], function(BaseController, UIComponent, LayoutType) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.cart.controller.Preferences", {
 		onInit: function () {
-			this._router = UIComponent.getRouterFor(this);
-		}
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("Preferences").attachMatched(this._onRouteMatched, this);
+
+		},
+		_onRouteMatched: function(oEvent) {
+				this.loadPreferences();
+			//	this.getModel("local").setProperty("/layout", LayoutType.TwoColumnsMidExpanded);
+				this.firstTwoDisplay();
+			},
+
+			loadPreferences: function(){
+				debugger;
+					var that = this;
+					this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+								"/Groups", "GET", {}, {}, this)
+								.then(function(oData) {
+					that.getOwnerComponent().getModel("local").setProperty("/list",{
+					Preferences:oData.results
+					});
+
+					})
+					.catch(function(oError) {
+							MessageToast.show("cannot fetch the data");
+					 });
+				}
 	});
 });
