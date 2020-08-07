ce5c7eae9af741224f64cfc1fa0fe0d5a47b1bb2
neha <nehapatil217@gmail.com>
2020-08-08 00:40:44 +1000

    Manufacturer screen fixes

--- client/controller/Manufacturer.controller.js
+++ client/controller/Manufacturer.controller.js
@@ -5,13 +5,13 @@ sap.ui.define([
   "sap/m/MessageToast",
   "sap/ui/demo/cart/model/formatter"
 ], function(BaseController, UIComponent, JSONModel,
-  MessageToast, formatter) {
+  MessageToast, Formatter) {
   "use strict";
   var manufacturerId;
   var changeCheck = 'false';
   var groupID = [];
   return BaseController.extend("sap.ui.demo.cart.controller.Manufacturer", {
-    formatter: formatter,
+    formatter: Formatter,
     onInit: function() {
       var oViewDetailModel = new JSONModel({
         "buttonText": "Save",
@@ -22,6 +22,7 @@ sap.ui.define([
 
       var oRouter = this.getRouter();
       oRouter.getRoute("Manufacturer").attachMatched(this._onRouteMatched, this);
+
       // this._router = UIComponent.getRouterFor(this);
     },
     _onRouteMatched: function(oEvent) {
@@ -38,16 +39,16 @@ sap.ui.define([
       });
       this.setModel(odataModel, "dataModel");
       debugger;
-      // this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
-      //     "/Manufacturers", "GET", {}, {}, this)
-      //   .then(function(oData) {
-      //     debugger;
-      //     var oModelManufacturer = new JSONModel();
-      //     oModelManufacturer.setData(oData);
-      //     that.getView().setModel(oModelManufacturer, "manufactureModelInfo");
-      //   }).catch(function(oError) {
-      //     MessageToast.show("cannot fetch the data");
-      //   });
+        this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+          "/Manufacturers", "GET", {}, {}, this)
+         .then(function(oData) {
+           debugger;
+           var oModelManufacturer = new JSONModel();
+           oModelManufacturer.setData(oData);
+           that.getView().setModel(oModelManufacturer, "manufactureModelInfo");
+         }).catch(function(oError) {
+           MessageToast.show("cannot fetch the data");
+         });
 
       this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
           "/Groups", "GET", {}, {}, this)
@@ -59,7 +60,7 @@ sap.ui.define([
           MessageToast.show("cannot fetch the data");
         });
       //
-      this.clearScreen();
+      //this.clearScreen();
     },
     clearScreen: function() {
       debugger;
@@ -88,6 +89,28 @@ sap.ui.define([
       dataModel.setProperty("/emailState", "None");
       this.getView().getModel("local").setProperty("/Manufacturer", manufacturerModel);
     },
+    manufacturerCheck1: function(code) {
+      var that = this;
+      var manufacturerJson = this.getView().getModel("manufactureModelInfo").getData().results;
+
+      function getmanufacturerDetail(code) {
+        return manufacturerJson.filter(
+          function(data) {
+            return (data.CustomerCode === code);
+          });
+      }
+      if (manufacturerJson && manufacturerJson.length > 0) {
+        var found = getmanufacturerDetail(code);
+        if (found.length > 0) {
+          var id = found[0].id;
+        }else{
+          var id = "";
+        }
+      }
+
+      return id;
+
+    },
     manufacturerCheck: function(code) {
       debugger;
       var that = this;
@@ -110,43 +133,47 @@ sap.ui.define([
           manufacturer.Name = found[0].Name;
           manufacturer.City = found[0].City;
           manufacturer.MobilePhone = found[0].MobilePhone;
+          manufacturer.Groups = found[0].Groups;
+          manufacturer.Categories = found[0].Categories;
           manufacturer.EmailId = found[0].EmailId;
+          manufacturer.Pattern = found[0].Pattern;
           if (found[0].Status === "B") {
             viewModel.setProperty("/blockStatus", true);
           } else if (found[0].Status === "U") {
             viewModel.setProperty("/blockStatus", false);
           }
+
           if (found[0].id) {
             var oFilter = new sap.ui.model.Filter({
               filters: [
                 new sap.ui.model.Filter("ManufacturerId",
-                  sap.ui.model.FilterOperator.Equals, found[0].id)
+                  sap.ui.model.FilterOperator.EQ, found[0].id)
               ]
             });
             this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
                 "/ManuGroups", "GET", oFilter, {}, this)
               .then(function(oData) {
-                that.getView().getModel("local").setProperty("/Manufacturer", oData.results[0]);
-                // for (var i = 0; i < oData.results.length; i++) {
-                //   var groupId = oData.results[i].GroupId;
-                //   var group = getGroupDetail(groupId);
-                //   debugger;
-                //   var seletedData = that.getView().getModel("local").getProperty("/groupSelected");
-                //   that.getView().getModel("local").setProperty("/groupSelected/GroupCode", group[i].groupCode);
-                //   that.getView().getModel("local").setProperty("/groupSelected/GroupId", group[i].id);
-                //   that.getView().getModel("local").setProperty("/groupSelected/ManuId", oData.results[i].id);
-                //   seletedData.GroupCode = group[i].groupCode;
-                //   seletedData.GroupId = group[i].id;
-                //   seletedData.ManuId = group[i].oData.results[i].id;
-                //   var oData = JSON.parse(JSON.stringify(seletedData));
-                //   groupID.push(oData);
-                // }
+                //that.getView().getModel("local").setProperty("/Manufacturer", oData.results[0]);
+                 for (var i = 0; i < oData.results.length; i++) {
+                   var groupId = oData.results[i].GroupId;
+                   var group = getGroupDetail(groupId);
+                   debugger;
+                   var seletedData = that.getView().getModel("local").getProperty("/groupSelected");
+                   that.getView().getModel("local").setProperty("/groupSelected/GroupCode", group[i].groupCode);
+                   that.getView().getModel("local").setProperty("/groupSelected/GroupId", group[i].id);
+                   that.getView().getModel("local").setProperty("/groupSelected/ManuId", oData.results[i].id);
+                   seletedData.GroupCode = group[i].groupCode;
+                   seletedData.GroupId = group[i].id;
+                   seletedData.ManuId = group[i].oData.results[i].id;
+                   var oData = JSON.parse(JSON.stringify(seletedData));
+                   groupID.push(oData);
+                 }
               })
               .catch(function(oError) {
                 // MessageToast.show("Data could not be saved");
               });
           }
-
+         //this.getView().getModel("local").setProperty("/Manufacturer", manufacturer);
           viewModel.setProperty("/buttonText", "Update");
           viewModel.setProperty("/deleteEnabled", true);
           viewModel.setProperty("/codeEnabled", false);
@@ -201,7 +228,10 @@ sap.ui.define([
           oSaveData.City = oSaveData.City.toUpperCase();
         }
         if (oSaveData.EmailId && oSaveData.EmailId !== "") {
-          oSaveData.EmailId = oSaveData.EmailId.toUpperCase();
+          if (Formatter.checkEmail(this.getView().byId("idEmail")) !== true){
+            return this.getView().byId("idEmail").setValueState("Error");
+          }
+        //  oSaveData.EmailId = oSaveData.EmailId.toUpperCase();
         }
 
         if (viewModel.oData.blockStatus === "true") {
@@ -209,12 +239,12 @@ sap.ui.define([
         } else {
           oSaveData.Status = "U";
         }
-        var id = this.manufacturerCheck(oSaveData.CustomerCode);
+        var id = this.manufacturerCheck1(oSaveData.CustomerCode);
         if (id) {
           var oFilter = new sap.ui.model.Filter({
             filters: [
               new sap.ui.model.Filter("ManufacturerId",
-                sap.ui.model.FilterOperator.Equals, id)
+                sap.ui.model.FilterOperator.EQ, id)
             ]
           });
           this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
@@ -222,7 +252,8 @@ sap.ui.define([
               oSaveData, this)
             .then(function(oData) {
               MessageToast.show("Data saved successfully");
-              that._onRouteMatched();
+              that.UpdateLocalModel();
+              that.clearScreen();
             }).catch(function(oError) {
               MessageToast.show("Data could not be saved");
             });
@@ -234,6 +265,17 @@ sap.ui.define([
               var status = that.updateGroupDetails(groupId, i, id);
             }
           }
+          //change user status
+          if (this.onSwitch === true){
+            debugger;
+              var changeUserStatus = {
+                emailId: oSaveData.EmailId,
+                name: oSaveData.Name,
+                bStat: viewModel.oData.blockStatus,
+                Authorization: this.getModel("local").getProperty("/Authorization")
+              };
+              this.changeUserStatus(changeUserStatus);
+          }
 
         } else {
           // var manufactureId = [];
@@ -241,18 +283,28 @@ sap.ui.define([
               "/Manufacturers", "POST", {}, oSaveData, this)
             .then(function(oData) {
               debugger;
-              var manufactureId = oData.id;
+          /*    var manufactureId = oData.id;
               for (var i = 1; i < groupId.length; i++) {
                 debugger;
                 var status = that.updateGroupDetails(groupId, i, manufactureId);
-              }
+              } */
               MessageToast.show("Data saved sucessfully");
-              // that._onRouteMatched();
+              that.clearScreen();
+              that._onRouteMatched();
             })
             .catch(function(oError) {
               MessageToast.show("Data could not be saved");
-              that._onRouteMatched();
+              //that._onRouteMatched();
             });
+            //Create new user
+            debugger;
+              var createUserPayload = {
+                name: oSaveData.Name,
+                emailId: oSaveData.EmailId,
+                role: "Maker",
+                Authorization: this.getModel("local").getProperty("/Authorization")
+              };
+          this.createUserPayload(createUserPayload);
 
         } //id check
       } else {
@@ -283,7 +335,8 @@ sap.ui.define([
             manuGroup.CreatedBy = "";
             manuGroup.CreatedOn = "";
             MessageToast.show("Data saved successfully");
-            that._onRouteMatched();
+            that.clearScreen();
+            //that._onRouteMatched();
           })
           .catch(function(oError) {
             manuGroup.ManufacturerId = "";
@@ -303,8 +356,9 @@ sap.ui.define([
       debugger;
       var status = oEvent.getParameters("Selected").state;
       // this.getView().getModel("local").setProperty("/Manufacturer/Block", status);
+      return this.onSwitch = true;
     },
-    checkEmail: function(oInput) {
+  /*  checkEmail: function(oInput) {
       if (oInput) {
         var email = oInput.getParameter("newValue");
         var dataModel = this.getView().getModel("dataModel");
@@ -316,7 +370,7 @@ sap.ui.define([
           this.getView().getModel("local").setProperty("/Manufacturer/EmailId", email);
         }
       }
-    },
+    },*/
     onSelectionChange: function(oEvent) {
       debugger;
       var oSrc = oEvent.getSource();
@@ -336,20 +390,14 @@ sap.ui.define([
     },
     CodeCheck: function(oEvent) {
       debugger;
-      var input_source = oEvent.getSource();
-      var id = input_source.getSelectedKey();
-      var text = input_source.getSelectedItem().getText();
+      var text = oEvent.getParameter("newValue");
       var viewModel = this.getView().getModel("viewModel");
-      // var manufactureId = this.manufacturerCheck(text);
-
-      debugger;
-      $('#idCode').keypress(function(event) {
-        if (event.keyCode == 13) {
-          $('#idName').focus();
-        }
-      });
+    //  var customerId = this.customerCheck(text);
+       var manufactureId = this.manufacturerCheck(text);
+      this.getView().byId("idName").focus();
     },
-    onEnter: function(oEvent){
+    /*onEnter: function(oEvent){
+      debugger;
       var that = this;
       var sValue = this.getView().byId("idCode").getValue().toUpperCase();
       this.getView().byId("idCode").setValue(sValue);
@@ -363,10 +411,10 @@ sap.ui.define([
               that.getView().getModel("local").setProperty("/Manufacturer",oData.results[0]);
         }else{
           MessageToast.show("Create as new Manufacturer");
-          that.getView().byId("idPName").focus();
         }
       });
-    },
+
+    },*/
     deleteManufacturer: function(oEvent) {
       debugger;
       var that = this;
@@ -391,6 +439,7 @@ sap.ui.define([
                     debugger
                   })
               }
+              that.clearScreen();
               that._onRouteMatched();
               MessageToast.show("Entry Deleted Sucessfully");
             })
@@ -403,6 +452,94 @@ sap.ui.define([
 
         }
       }
-    }
+    },
+    onNameEnt: function(oEvent) {
+      this.getView().byId("idName").setValue(oEvent.getParameter("value").toUpperCase());
+      this.getView().byId("idAddress").focus();
+    },
+    onAddrEnt: function(oEvent) {
+      this.getView().byId("idCity").focus();
+    },
+    onCityEnt: function(oEvent) {
+      this.getView().byId("idContact").focus();
+    },
+    onContEnt: function(oEvent) {
+      this.getView().byId("idgpCode").focus();
+    },
+    onPatternEnt: function(oEvent) {
+      this.getView().byId("idBlock").focus();
+    },
+    onEmailEnt: function(oEvent) {
+      this.getView().byId("idPattern").focus();
+    },
+    onSelectionFinish:function(){
+      debugger;
+      this.getView().byId("idCatCode").focus();
+    },
+    onSelectionFinish1:function(){
+      debugger;
+      this.getView().byId("idEmail").focus();
+    },
+    createUserPayload:function(createUserPayload){
+      debugger;
+
+      $.post('/createNewUser', createUserPayload)
+      .then(function(data){
+        debugger;
+      })
+      .fail(function(error) {
+        sap.m.MessageBox.error("User Creation failed");
+      });
+    },
+    changeUserStatus:function(changeUserStatus){
+      debugger;
+
+      $.post('/changeUserStatus', changeUserStatus)
+      .then(function(data){
+        debugger;
+      })
+      .fail(function(error) {
+        sap.m.MessageBox.error("Changing User Status failed");
+      });
+    },
+    UpdateLocalModel:function(data){
+      debugger;
+        var that = this;
+        var code = this.getView().byId("idCode").getValue();
+        var manufactureJson = this.getView().getModel("manufactureModelInfo").getData().results;
+
+        function getmanufactureDetail(code) {
+          return manufactureJson.filter(
+            function(data) {
+              return (data.CustomerCode === code);
+            });
+        }
+        if (manufactureJson && manufactureJson.length > 0) {
+          var manufactureModelInfo = getmanufactureDetail(code);
+          if (manufactureModelInfo.length > 0) {
+            var viewModel = this.getView().getModel("viewModel");
+            var status = viewModel.getProperty("/blockStatus");
+            var omanufacture = this.getView().getModel("local").getProperty("/Manufacturer");
+
+             manufactureModelInfo[0].CustomerCode = omanufacture.CustomerCode;
+             manufactureModelInfo[0].Address = omanufacture.Address;
+             manufactureModelInfo[0].Name = omanufacture.Name;
+             manufactureModelInfo[0].City = omanufacture.City ;
+             manufactureModelInfo[0].MobilePhone = omanufacture.MobilePhone;
+             manufactureModelInfo[0].EmailId = omanufacture.EmailId;
+             manufactureModelInfo[0].Groups = omanufacture.Groups;
+             manufactureModelInfo[0].Categories = omanufacture.Categories;
+             manufactureModelInfo[0].Pattern = omanufacture.Pattern;
+             var group = this.getView().getModel("local").getProperty("/groupSelected");
+
+             debugger;
+             if (status === true) {
+               manufactureModelInfo[0].Status = "B" ;
+             } else if (status === false) {
+               manufactureModelInfo[0].Status = "U" ;
+             }
+           }
+          }
+        }
   });
 });
