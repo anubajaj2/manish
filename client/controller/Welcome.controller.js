7243b3bc6707ff5b93191d7dd25f533f24596054
neha <nehapatil217@gmail.com>
2020-08-13 13:05:12 +1000

    ProductSearch Fixes

--- client/controller/Welcome.controller.js
+++ client/controller/Welcome.controller.js
@@ -23,37 +23,54 @@ sap.ui.define([
 		onSearch: function(){
 			var oView = this.getView();
 			var aFilter = [];
+			var orFilter = [];
 			debugger;
 			//read categories selected
 			var aItems = oView.byId("gridList").getItems();
 			for (var i = 0; i < aItems.length; i++) {
 				if(aItems[i].hasStyleClass("colorGreen")){
-					aFilter.push(new Filter("Category", FilterOperator.EQ ,
+					aFilter.push(new Filter("Category",FilterOperator.EQ,
 					aItems[i].getContent()[0].getItems()[0].getItems()[0].getText()
 					));
 				}
+			}
+			if ( aFilter.length > 0 ){
+				orFilter.push(new Filter(aFilter, false));
+				aFilter = [];
 			}
 			//read sub categories
 			var aItems2 = this.getView().getParent().getParent().getBeginColumnPages()[0].getContent()[0].getContent()[0].getItems();
 			for (var i = 0; i < aItems2.length; i++) {
 				if(aItems2[i].hasStyleClass("colorGreen")){
-					aFilter.push(new Filter("SubCategory", FilterOperator.EQ ,
+					aFilter.push(new Filter("SubCategory",FilterOperator.EQ,
 					aItems2[i].getLabel()
 					));
 				}
 			}
+			if ( aFilter.length > 0 ){
+				orFilter.push(new Filter(aFilter, false));
+				aFilter = [];
+			}
 			//read range Value low and high
 			var aRange = oView.byId("range").getRange();
 			aFilter.push(new Filter("GrossWeight",FilterOperator.BT, aRange[0], aRange[1]));
+
+			orFilter.push(new Filter(aFilter));
+			aFilter = [];
 			//read the type
 			if(oView.byId("togPlain").getPressed()){
-				aFilter.push(new Filter("Type",FilterOperator.EQ,"PLAIN"));
+				aFilter.push(new Filter("Type",FilterOperator.EQ,"P"));
 			}
 			if(oView.byId("togStudded").getPressed()){
-				aFilter.push(new Filter("Type",FilterOperator.EQ,"STUDDED"));
+				aFilter.push(new Filter("Type",FilterOperator.EQ,"S"));
 			}
+			if ( aFilter.length > 0 ){
+				orFilter.push(new Filter(aFilter, false));
+				aFilter = [];
+			}
+
 			var oFilter = new Filter({
-				filters: aFilter,
+				filters: orFilter,
 				and:true
 			});
 			//set filter
