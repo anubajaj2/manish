a1c02ce999e264d0f3d7103d0c25ffd2a1395a05
yogendra-code <yogendra@evotrainingsolutions.com>
2020-10-02 18:32:47 +0530

    server

--- server/server.js
+++ server/server.js
@@ -50,16 +50,6 @@ app.start = function() {
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
-  return app.listen(function() {
-		app.emit('started');
-		var baseUrl = app.get('url').replace(/\/$/, '');
-		console.log('Web server listening at: %s', baseUrl);
-		if (app.get('loopback-component-explorer')) {
-			var explorerPath = app.get('loopback-component-explorer').mountPath;
-			console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-		}
-  });
-
 };
 
 
