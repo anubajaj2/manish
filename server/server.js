700eba457d490bf4d3b407514f24d8db8f907311
yogendra-code <yogendra@evotrainingsolutions.com>
2020-10-02 23:13:26 +0530

    sever

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-// var https = require('https');
+var https = require('https');
 var http = require('http');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -15,10 +15,10 @@ var invoicegenerator = require('./invoice-generator');
 var app = express();
 app = module.exports = loopback();
 
-// var options = {
-//   key: fs.readFileSync(path.join(__dirname, './cert/key.pem')).toString(),
-//   cert: fs.readFileSync(path.join(__dirname, './cert/cert.pem')).toString()
-// };
+var options = {
+  key: fs.readFileSync(path.join(__dirname, './cert/key.pem')).toString(),
+  cert: fs.readFileSync(path.join(__dirname, './cert/cert.pem')).toString()
+};
 // parse application/json
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({
@@ -35,30 +35,40 @@ app.use(session({
   secret: 'anuragApp'
 }));
 app.use(fileUpload());
+app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+});
 
 app.start = function() {
   // start the web server
-  // const sslServer = https.createServer(options,app);
-  //
-  // sslServer.listen(app.get('port'),function() {
-  //   app.emit('started');
-  //   // var baseUrl = app.get('url').replace(/\/$/, '');
-  //   var baseUrl = app.get('url');
-  //   console.log('Web server listening at: %s', baseUrl);
-  //   if (app.get('loopback-component-explorer')) {
-  //     var explorerPath = app.get('loopback-component-explorer').mountPath;
-  //     console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-  //   }
-  // });
-  return app.listen(function() {
-		app.emit('started');
-		var baseUrl = app.get('url').replace(/\/$/, '');
-		console.log('Web server listening at: %s', baseUrl);
-		if (app.get('loopback-component-explorer')) {
-			var explorerPath = app.get('loopback-component-explorer').mountPath;
-			console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-		}
+  const sslServer = https.createServer(options,app);
+
+  sslServer.listen(app.get('port'),function() {
+    app.emit('started');
+    // var baseUrl = app.get('url').replace(/\/$/, '');
+    var baseUrl = app.get('url');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
+  http.createServer(app).listen(80);
+  // return app.listen(function() {
+	// 	app.emit('started');
+	// 	var baseUrl = app.get('url').replace(/\/$/, '');
+	// 	console.log('Web server listening at: %s', baseUrl);
+	// 	if (app.get('loopback-component-explorer')) {
+	// 		var explorerPath = app.get('loopback-component-explorer').mountPath;
+	// 		console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+	// 	}
+  // });
 
 };
 
