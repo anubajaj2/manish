9f7331dd97e21c2a6296c1d76d7af2a21e610eb7
yogendra-code <yogendrakumar@soyuztechnologies.com>
2021-06-16 09:42:06 +0530

    categories view

---
+++ src/webapp/controller/HomeOLD.controller.js
@@ -0,0 +1,124 @@
+sap.ui.define([
+	"sap/ui/demo/cart/controller/BaseController",
+	"sap/ui/demo/cart/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/Device",
+	"sap/ui/model/json/JSONModel"
+], function (
+	BaseController,
+	formatter,
+	Filter,
+	FilterOperator,
+	Device,
+	JSONModel,
+) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.demo.cart.controller.Home", {
+		formatter : formatter,
+		onInit: function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("home").attachMatched(this._onRouteMatched, this);
+
+		},
+		_firstTime: true,
+		_onRouteMatched: function(oEvent) {
+				this.loadCategories();
+				this.firstTwoDisplay();
+				if(this._oPopover){
+					this.closePopover();
+				}
+		},
+
+		onSearch: function () {
+			this._search();
+		},
+		onGridItemPress: function(oEvent) {
+			//alert("ay");
+			if(oEvent.getParameter("listItem").hasStyleClass("colorGreen")){
+					oEvent.getParameter("listItem").removeStyleClass("colorGreen");
+			}else{
+				oEvent.getParameter("listItem").addStyleClass("colorGreen");
+			}
+
+		},
+		onRefresh: function () {
+			debugger;
+			// trigger search again and hide pullToRefresh when data ready
+			var oProductList = this.byId("productList");
+			var oBinding = oProductList.getBinding("items");
+			var fnHandler = function () {
+				this.byId("pullToRefresh").hide();
+				oBinding.detachDataReceived(fnHandler);
+			}.bind(this);
+			oBinding.attachDataReceived(fnHandler);
+			this._search();
+		},
+
+		_search: function () {
+			debugger;
+			var oView = this.getView();
+			var oProductList = oView.byId("productList");
+			var oCategoryList = oView.byId("categoryList");
+			var oSearchField = oView.byId("searchField");
+
+			// switch visibility of lists
+			var bShowSearchResults = oSearchField.getValue().length !== 0;
+			oProductList.setVisible(bShowSearchResults);
+			oCategoryList.setVisible(!bShowSearchResults);
+
+			// filter product list
+			var oBinding = oProductList.getBinding("items");
+			if (oBinding) {
+				if (bShowSearchResults) {
+					var oFilter = new Filter("Name", FilterOperator.Contains, oSearchField.getValue());
+					oBinding.filter([oFilter]);
+				} else {
+					oBinding.filter([]);
+				}
+			}
+		},
+
+		onCategoryListItemPress: function (oEvent) {
+			var oBindContext = oEvent.getSource().getBindingContext();
+			var oModel = oBindContext.getModel();
+			var sCategoryId = oModel.getData(oBindContext.getPath()).id;
+
+			this._router.navTo("category", {id: sCategoryId});
+
+			this._unhideMiddlePage();
+		},
+
+		onProductListSelect: function (oEvent) {
+			debugger;
+			var oItem = oEvent.getParameter("listItem");
+			this._showProduct(oItem);
+		},
+
+		onProductListItemPress: function (oEvent) {
+			debugger;
+			var oItem = oEvent.getSource();
+			this._showProduct(oItem);
+		},
+
+		_showProduct: function (oItem) {
+			debugger;
+			var oEntry = oItem.getBindingContext().getObject();
+
+			this._router.navTo("product", {
+				id: oEntry.Category,
+				productId: oEntry.ProductId
+			}, !Device.system.phone);
+		},
+
+		/**
+		 * Always navigates back to home
+		 * @override
+		 */
+		onBack: function () {
+			debugger;
+			this.getRouter().navTo("home");
+		}
+	});
+});
--- src/webapp/controller/Welcome.controller.js
+++ src/webapp/controller/Welcome.controller.js
@@ -1,114 +1,158 @@
 sap.ui.define([
-	"sap/ui/demo/cart/controller/BaseController",
-	"sap/ui/demo/cart/model/cart",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/Filter",
-	"sap/ui/demo/cart/model/formatter",
-	"sap/ui/model/FilterOperator"
-], function (BaseController, cart, JSONModel, Filter, formatter, FilterOperator) {
-	"use strict";
+  "sap/ui/demo/cart/controller/BaseController",
+  "sap/ui/demo/cart/model/cart",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/Filter",
+  "sap/ui/demo/cart/model/formatter",
+  "sap/ui/model/FilterOperator",
+	"sap/m/Popover",
+	"sap/m/Button",
+	"sap/m/library"
+], function(BaseController, cart, JSONModel, Filter, formatter, FilterOperator, Popover, Button, library) {
+  "use strict";
+  var ButtonType = library.ButtonType,
+    PlacementType = library.PlacementType;
+  return BaseController.extend("sap.ui.demo.cart.controller.Welcome", {
+    formatter: formatter,
+    onGridItemPress: function(oEvent) {
+      //alert("ay");
+      if (oEvent.getParameter("listItem").hasStyleClass("colorGreen")) {
+        oEvent.getParameter("listItem").removeStyleClass("colorGreen");
+      } else {
+        oEvent.getParameter("listItem").addStyleClass("colorGreen");
+      }
+      console.log(oEvent.getParameter("listItem").getContent()[0].getItems()[0].getItems()[0].getText());
 
-	return BaseController.extend("sap.ui.demo.cart.controller.Welcome", {
-		formatter: formatter,
-		onGridItemPress: function(oEvent) {
-			//alert("ay");
-			if(oEvent.getParameter("listItem").hasStyleClass("colorGreen")){
-					oEvent.getParameter("listItem").removeStyleClass("colorGreen");
-			}else{
-				oEvent.getParameter("listItem").addStyleClass("colorGreen");
-			}
-			console.log(oEvent.getParameter("listItem").getContent()[0].getItems()[0].getItems()[0].getText());
+    },
+    onSearch: function() {
+      var oView = this.getView();
+      var aFilter = [];
+      var orFilter = [];
+      debugger;
+      //read categories selected
+      var aItems = oView.byId("gridList").getItems();
+      for (var i = 0; i < aItems.length; i++) {
+        if (aItems[i].hasStyleClass("colorGreen")) {
+          aFilter.push(new Filter("Category", FilterOperator.EQ,
+            aItems[i].getContent()[0].getItems()[0].getItems()[0].getText()
+          ));
+        }
+      }
+      if (aFilter.length > 0) {
+        orFilter.push(new Filter(aFilter, false));
+        aFilter = [];
+      }
+      //read sub categories
+      var aItems2 = this.getView().getParent().getParent().getBeginColumnPages()[0].getContent()[0].getContent()[0].getItems();
+      for (var i = 0; i < aItems2.length; i++) {
+        if (aItems2[i].hasStyleClass("colorGreen")) {
+          aFilter.push(new Filter("SubCategory", FilterOperator.EQ,
+            aItems2[i].getLabel()
+          ));
+        }
+      }
+      if (aFilter.length > 0) {
+        orFilter.push(new Filter(aFilter, false));
+        aFilter = [];
+      }
+      //read range Value low and high
+      var aRange = oView.byId("range").getRange();
+      aFilter.push(new Filter("GrossWeight", FilterOperator.BT, aRange[0], aRange[1]));
 
-		},
-		onSearch: function(){
-			var oView = this.getView();
-			var aFilter = [];
-			var orFilter = [];
-			debugger;
-			//read categories selected
-			var aItems = oView.byId("gridList").getItems();
-			for (var i = 0; i < aItems.length; i++) {
-				if(aItems[i].hasStyleClass("colorGreen")){
-					aFilter.push(new Filter("Category",FilterOperator.EQ,
-					aItems[i].getContent()[0].getItems()[0].getItems()[0].getText()
-					));
-				}
-			}
-			if ( aFilter.length > 0 ){
-				orFilter.push(new Filter(aFilter, false));
-				aFilter = [];
-			}
-			//read sub categories
-			var aItems2 = this.getView().getParent().getParent().getBeginColumnPages()[0].getContent()[0].getContent()[0].getItems();
-			for (var i = 0; i < aItems2.length; i++) {
-				if(aItems2[i].hasStyleClass("colorGreen")){
-					aFilter.push(new Filter("SubCategory",FilterOperator.EQ,
-					aItems2[i].getLabel()
-					));
-				}
-			}
-			if ( aFilter.length > 0 ){
-				orFilter.push(new Filter(aFilter, false));
-				aFilter = [];
-			}
-			//read range Value low and high
-			var aRange = oView.byId("range").getRange();
-			aFilter.push(new Filter("GrossWeight",FilterOperator.BT, aRange[0], aRange[1]));
+      orFilter.push(new Filter(aFilter));
+      aFilter = [];
 
-			orFilter.push(new Filter(aFilter));
-			aFilter = [];
+      aFilter.push(new Filter("ProdStatus", FilterOperator.EQ, "A"));
 
-			aFilter.push(new Filter("ProdStatus",FilterOperator.EQ, "A"));
+      orFilter.push(new Filter(aFilter));
+      aFilter = [];
+      //read the type
+      if (oView.byId("togPlain").getPressed()) {
+        aFilter.push(new Filter("Type", FilterOperator.EQ, "P"));
+      }
+      if (oView.byId("togStudded").getPressed()) {
+        aFilter.push(new Filter("Type", FilterOperator.EQ, "S"));
+      }
+      if (aFilter.length > 0) {
+        orFilter.push(new Filter(aFilter, false));
+        aFilter = [];
+      }
 
-			orFilter.push(new Filter(aFilter));
-			aFilter = [];
-			//read the type
-			if(oView.byId("togPlain").getPressed()){
-				aFilter.push(new Filter("Type",FilterOperator.EQ,"P"));
-			}
-			if(oView.byId("togStudded").getPressed()){
-				aFilter.push(new Filter("Type",FilterOperator.EQ,"S"));
-			}
-			if ( aFilter.length > 0 ){
-				orFilter.push(new Filter(aFilter, false));
-				aFilter = [];
-			}
+      var oFilter = new Filter({
+        filters: orFilter,
+        and: true
+      });
+      //set filter
+      this.getOwnerComponent().getModel("local").setProperty("/searchFilter", oFilter);
 
-			var oFilter = new Filter({
-				filters: orFilter,
-				and:true
-			});
-			//set filter
-			this.getOwnerComponent().getModel("local").setProperty("/searchFilter", oFilter);
+      this.getRouter().navTo("productSearch");
 
-			this.getRouter().navTo("productSearch");
+    },
+    onClear: function() {
 
-		},
-		onClear: function(){
+    },
+    onInit: function() {
+      this.oRouter = this.getOwnerComponent().getRouter();
+      this.oRouter.getRoute("categories").attachMatched(this._onRouteMatched, this);
+      // this.loadCategories();
+      // setTimeout(this._initLoad.bind(this),1000);
+    },
+    // _initLoad: function(){
+    // 	var oPage = this.getView().byId("myPage");
+    // 	var allBtn = this.getOwnerComponent().getModel("local").getProperty("/cat/category");
+    // 	for (var i = 0; i < allBtn.length; i++) {
+    // 		var text = allBtn[i].Category;
+    // 		oPage.addContent(new sap.m.ToggleButton({
+    // 			text: text
+    //
+    // 		}).addStyleClass('class','sapUiResponsiveMargin sapUiLargeMargin sapUiLargePadding'));
+    // 	}
+    // },
+    onSideNavButtonPress: function() {
+      var oToolPage = this.byId("toolPage");
+      var bSideExpanded = oToolPage.getSideExpanded();
 
-		},
-		onInit: function () {
-			this.oRouter = this.getOwnerComponent().getRouter();
-			this.oRouter.getRoute("home").attachMatched(this._onRouteMatched, this);
-			// this.loadCategories();
-			// setTimeout(this._initLoad.bind(this),1000);
-		},
-		// _initLoad: function(){
-		// 	var oPage = this.getView().byId("myPage");
-		// 	var allBtn = this.getOwnerComponent().getModel("local").getProperty("/cat/category");
-		// 	for (var i = 0; i < allBtn.length; i++) {
-		// 		var text = allBtn[i].Category;
-		// 		oPage.addContent(new sap.m.ToggleButton({
-		// 			text: text
+      this._setToggleButtonTooltip(bSideExpanded);
+
+      oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+    },
+    _setToggleButtonTooltip: function(bLarge) {
+      var oToggleButton = this.byId('sideNavigationToggleButton');
+      if (bLarge) {
+        oToggleButton.setTooltip('Large Size Navigation');
+      } else {
+        oToggleButton.setTooltip('Small Size Navigation');
+      }
+    },
+    // handleUserNamePress: function(event) {
+		// 	var that = this;
+    //   var oPopover = new Popover({
+    //     showHeader: false,
+    //     placement: PlacementType.Bottom,
+    //     content: [
+    //       new Button({
+    //         text: 'Feedback',
+    //         type: ButtonType.Transparent
+    //       }),
+    //       new Button({
+    //         text: 'Help',
+    //         type: ButtonType.Transparent
+    //       }),
+    //       new Button({
+    //         text: 'Logout',
+    //         type: ButtonType.Transparent,
+		// 				press: that.logOutApp()
+    //       })
+    //     ]
+    //   }).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
 		//
-		// 		}).addStyleClass('class','sapUiResponsiveMargin sapUiLargeMargin sapUiLargePadding'));
-		// 	}
-		// },
-		_onRouteMatched: function (oEvent) {
-			//this.firstTwoDisplay();
-		},
-		onNavButtonPress: function(){
-			this.oRouter.navTo("CustomerLanding");
-		}
-	});
+    //   oPopover.openBy(event.getSource());
+    // },
+    _onRouteMatched: function(oEvent) {
+      this.loadCategories();
+    },
+    onNavButtonPress: function() {
+      this.oRouter.navTo("CustomerLanding");
+    }
+  });
 });
