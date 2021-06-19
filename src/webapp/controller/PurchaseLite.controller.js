b2046836f3798e728a81cf99ede149a9fcf97ed1
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-06-20 00:50:19 +0530

    edit,Delete,update,excelTemplateDownload with data

--- src/webapp/controller/PurchaseLite.controller.js
+++ src/webapp/controller/PurchaseLite.controller.js
@@ -10,9 +10,9 @@ sap.ui.define([
   "sap/m/SelectDialog",
   "sap/ui/export/library",
   "sap/ui/export/Spreadsheet",
-	"sap/ui/core/Fragment",
+  "sap/ui/core/Fragment",
   "sap/m/Dialog",
-	"sap/ui/unified/FileUploader",
+  "sap/ui/unified/FileUploader",
 ], function(BaseController, UIComponent, JSONModel,
   MessageToast, Formatter, MessageBox, Filter, FilterOperator, SelectDialog, exportLibrary, Spreadsheet,Fragment, Dialog, FileUploader) {
 	"use strict";
@@ -20,6 +20,12 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.cart.controller.PurchaseLite", {
 		onInit: function () {
 			this._router = UIComponent.getRouterFor(this);
+			var odataModel = new JSONModel({
+        "groupCodeState": "None",
+        "emailState": "None",
+        "PatternState": "None"
+      });
+      this.setModel(odataModel, "PurchaseLiteModel");
 		},
 		onAddExcelData:function(){
     debugger;
@@ -69,20 +75,22 @@ sap.ui.define([
 					for (var i = 0; i < excelData.length; i++) {
 
 						excelD.push({
+							ItemCode:"***",
 							TagNo: excelData[i]["TAG NO"],
 							GWt: excelData[i]["G WT"],
 							Amount: excelData[i]["Amount"],
 							PCS: excelData[i]["PCS"],
 							Size: excelData[i]["Size"],
-							Remark: excelData[i]["Remark"]
+							Remark: excelData[i]["Remark"],
+							Photo:""
 						});
 
 					}
 					debugger;
 					// Setting the data to the local model
 
-					that.getView().getModel("dataModel").setProperty("/PurchaseLite",excelD);
-          that.getView().getModel("dataModel").setProperty("/title",excelD.length);
+					that.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite",excelD);
+          that.getView().getModel("PurchaseLiteModel").setProperty("/title",excelD.length);
 				};
 				reader.onerror = function(ex) {
 					console.log(ex);
@@ -90,6 +98,165 @@ sap.ui.define([
 				reader.readAsBinaryString(file);
 				this.fixedDialog.close();
 			}
+		},
+		onAdd:function(){
+			debugger;
+			var oNew ={
+						ItemCode:"***",
+						TagNo: "",
+						GWt: "",
+						Amount: "",
+						PCS: "",
+						Size: "",
+						Remark: "",
+						Photo:""
+					};
+			this.getView().getModel("PurchaseLiteModel").setProperty("/entry",oNew);
+			var that=this;
+			if(!this.oAddProduct){
+			Fragment.load({
+					id: "idAddProduct",
+					name: "sap.ui.demo.cart.fragments.PurchaseStyleCURD",
+					controller: this
+				}).then(function(oPopup) {
+					that.oAddProduct = oPopup;
+					that.getView().addDependent(oPopup);
+					oPopup.setTitle("ADD");	
+					oPopup.open();
+				});
+			}
+			else{
+				this.oAddProduct.open();
+			}
+		},
+		onEdit:function(oEvent){
+			debugger;
+			var that=this;
+			var sPath=oEvent.getSource().getParent().getParent().getRowBindingContext().getPath();
+			var value=this.getView().getModel("PurchaseLiteModel").getProperty(sPath);
+			this.getView().getModel("PurchaseLiteModel").setProperty("/entry",value);
+			if(!this.oEditProduct){
+			Fragment.load({
+					id: "idEditProduct",
+					name: "sap.ui.demo.cart.fragments.PurchaseStyleCURD",
+					controller: this
+				}).then(function(oPopup) {
+					that.oEditProduct = oPopup;
+					that.getView().addDependent(oPopup);
+					oPopup.setTitle("ADD");	
+					oPopup.open();
+				});
+			}
+			else{
+				this.oEditProduct.open();
+			}
+		},
+		onCopy:function(oEvent){
+			debugger;
+			var that=this;
+			var sPath=oEvent.getSource().getParent().getParent().getRowBindingContext().getPath();
+			var value=this.getView().getModel("PurchaseLiteModel").getProperty(sPath);
+			this.getView().getModel("PurchaseLiteModel").setProperty("/entry",value);
+			if(!this.oCopyProduct){
+			Fragment.load({
+					id: "idCopyProduct",
+					name: "sap.ui.demo.cart.fragments.PurchaseStyleCURD",
+					controller: this
+				}).then(function(oPopup) {
+					that.oCopyProduct = oPopup;
+					that.getView().addDependent(oPopup);
+					oPopup.setTitle("ADD");	
+					oPopup.open();
+				});
+			}
+			else{
+				this.oCopyProduct.open();
+			}
+		},
+		onPressHandleSecureCancelPopup:function(oEvent){
+			debugger;
+			oEvent.getSource().getParent().close();
+			// this.oAddProduct.close();
+		},
+		onPressHandleSecureOkPopup:function(oEvent){
+			var sId=oEvent.getSource().getParent().getId().split("--")[0];
+			if(sId==="idAddProduct"){
+				debugger;
+				MessageToast.show("Under Construction...");
+
+			}
+			else if(sId==="idEditProduct"){
+				debugger;
+				MessageToast.show("Under Construction...");
+			}
+			else if(sId==="idCopyProduct"){
+				debugger;
+				MessageToast.show("Under Construction...");
+			}
+			// this.oAddProduct.close();
+			oEvent.getSource().getParent().close();
+		},
+		onPressTemplateDownload:function(){
+			debugger;
+			var aCols, aMessages, oSettings;
+			// if (Array.isArray(exportData) && exportData.length) {
+				aCols = this._createExcelColumns();
+				var reportExcel = [];
+				for(var i=1;i<51;i++){
+					reportExcel.push({
+						ItemCode:"***",
+						TagNo: "T-"+i,
+						GWt: i*10,
+						Amount: (i*10)+(i*5),
+						PCS: i,
+						Size: i,
+						Remark: i,
+						Photo:""
+					});
+				}
+				// reportExcel.push("TEMPLATE");
+				oSettings = {
+					workbook: {
+						columns: aCols
+					},
+					dataSource: reportExcel
+				};
+				new Spreadsheet(oSettings)
+					.build()
+					.then(function() {
+						MessageToast.show("Spreadsheet export has finished");
+					});
+			// }
+		},
+		_createExcelColumns:function(){
+			return [
+				// {
+				// 	label: 'Action',
+				// 	property: 'Action',
+				// 	width: '25'
+				// }, 
+				{
+					label: 'TAG NO',
+					property: 'TagNo'
+
+				}, {
+					label: 'G WT',
+					property: 'GWt'
+				}, {
+					label: 'Amount',
+					property: 'Amount'
+				}, {
+					label: 'PCS',
+					property: 'PCS'
+
+				}, {
+					label: 'Size',
+					property: 'Size'
+				}, {
+					label: 'Remark',
+					property: 'Remark'
+				}
+			];
 		}
 	});
 });
