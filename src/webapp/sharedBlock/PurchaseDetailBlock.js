ca3512fda9d0d4df5cf3c6a73485500fadf831d8
kuldeep singh <kuldeepsingh@soyuztechnologies.com>
2021-06-19 18:17:02 +0530

    replicate code of maker screen

---
+++ src/webapp/sharedBlock/PurchaseDetailBlock.js
@@ -0,0 +1,21 @@
+sap.ui.define(["sap/ui/core/library", 'sap/uxap/BlockBase'], function(coreLibrary, BlockBase) {
+	"use strict";
+
+	var ViewType = coreLibrary.mvc.ViewType;
+
+	var PurchaseDetailBlock = BlockBase.extend("studentportal.sharedBlock.PurchaseDetailBlock", {
+		metadata: {
+			views: {
+				Collapsed: {
+					viewName: "sap.ui.demo.cart.view.PurchaseDetail",
+					type: ViewType.XML
+				},
+				Expanded: {
+					viewName: "sap.ui.demo.cart.view.PurchaseDetail",
+					type: ViewType.XML
+				}
+			}
+		}
+	});
+	return PurchaseDetailBlock;
+});
---
+++ src/webapp/sharedBlock/PurchaseLeingthBlock.js
@@ -0,0 +1,21 @@
+sap.ui.define(["sap/ui/core/library", 'sap/uxap/BlockBase'], function(coreLibrary, BlockBase) {
+	"use strict";
+
+	var ViewType = coreLibrary.mvc.ViewType;
+
+	var PurchaseLeingthBlock = BlockBase.extend("studentportal.sharedBlock.PurchaseLeingthBlock", {
+		metadata: {
+			views: {
+				Collapsed: {
+					viewName: "sap.ui.demo.cart.view.PurchaseLeingth",
+					type: ViewType.XML
+				},
+				Expanded: {
+					viewName: "sap.ui.demo.cart.view.PurchaseLeingth",
+					type: ViewType.XML
+				}
+			}
+		}
+	});
+	return PurchaseLeingthBlock;
+});
