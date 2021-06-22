9f682a33dc7badbc3b97c4fbca19a090798bd116
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-06-22 14:23:56 +0530

    Image upload mass and single

--- src/webapp/controller/PurchaseLite.controller.js
+++ src/webapp/controller/PurchaseLite.controller.js
@@ -2,35 +2,35 @@ sap.ui.define([
 	"sap/ui/demo/cart/controller/BaseController",
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-  "sap/m/MessageToast",
-  "sap/ui/demo/cart/model/formatter",
-  "sap/m/MessageBox",
-  "sap/ui/model/Filter",
-  "sap/ui/model/FilterOperator",
-  "sap/m/SelectDialog",
-  "sap/ui/export/library",
-  "sap/ui/export/Spreadsheet",
-  "sap/ui/core/Fragment",
-  "sap/m/Dialog",
-  "sap/ui/unified/FileUploader",
+	"sap/m/MessageToast",
+	"sap/ui/demo/cart/model/formatter",
+	"sap/m/MessageBox",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/SelectDialog",
+	"sap/ui/export/library",
+	"sap/ui/export/Spreadsheet",
+	"sap/ui/core/Fragment",
+	"sap/m/Dialog",
+	"sap/ui/unified/FileUploader",
 ], function(BaseController, UIComponent, JSONModel,
-  MessageToast, Formatter, MessageBox, Filter, FilterOperator, SelectDialog, exportLibrary, Spreadsheet,Fragment, Dialog, FileUploader) {
+	MessageToast, Formatter, MessageBox, Filter, FilterOperator, SelectDialog, exportLibrary, Spreadsheet, Fragment, Dialog, FileUploader) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.cart.controller.PurchaseLite", {
-		onInit: function () {
+		onInit: function() {
 			this._router = UIComponent.getRouterFor(this);
 			var odataModel = new JSONModel({
-        "groupCodeState": "None",
-        "emailState": "None",
-        "PatternState": "None"
-      });
-      this.setModel(odataModel, "PurchaseLiteModel");
+				"groupCodeState": "None",
+				"emailState": "None",
+				"PatternState": "None"
+			});
+			this.setModel(odataModel, "PurchaseLiteModel");
 		},
-		onAddExcelData:function(){
-    debugger;
-    var that=this;
-    	if (this.fixedDialog === undefined) {
+		onAddExcelData: function() {
+			debugger;
+			var that = this;
+			if (this.fixedDialog === undefined) {
 				this.fixedDialog = new Dialog({
 					title: "Choose XSLX File For Upload",
 					width: "60%",
@@ -49,13 +49,14 @@ sap.ui.define([
 					]
 				});
 				this.getView().addDependent(this.fixedDialog);
-        	}
+			}
 			this.fixedDialog.open();
-  },
-  onUpload: function(e) {
+		},
+
+		onUpload: function(e) {
 			this._import(e.getParameter("files") && e.getParameter("files")[0]);
 		},
-    		_import: function(file) {
+		_import: function(file) {
 			var that = this;
 			var excelData = {};
 			if (file && window.FileReader) {
@@ -75,23 +76,23 @@ sap.ui.define([
 					for (var i = 0; i < excelData.length; i++) {
 
 						excelD.push({
-							ItemCode:"***",
+							ItemCode: "***",
 							TagNo: excelData[i]["TAG NO"],
 							GWt: excelData[i]["G WT"],
 							Amount: excelData[i]["Amount"],
 							PCS: excelData[i]["PCS"],
 							Size: excelData[i]["Size"],
 							Remark: excelData[i]["Remark"],
-							Photo:[],
-							PhotoCheck:false
+							Photo: [],
+							PhotoCheck: false
 						});
 
 					}
 					debugger;
 					// Setting the data to the local model
 
-					that.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite",excelD);
-          that.getView().getModel("PurchaseLiteModel").setProperty("/title",excelD.length);
+					that.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite", excelD);
+					that.getView().getModel("PurchaseLiteModel").setProperty("/title", excelD.length);
 				};
 				reader.onerror = function(ex) {
 					console.log(ex);
@@ -100,45 +101,115 @@ sap.ui.define([
 				this.fixedDialog.close();
 			}
 		},
-		onAdd:function(){
+		onAddImageData: function() {
 			debugger;
-			var oNew ={
-						ItemCode:"***",
-						TagNo: "",
-						GWt: "",
-						Amount: "",
-						PCS: "",
-						Size: "",
-						Remark: "",
-						Photo:[],
-						PhotoCheck:false
-					};
-			this.getView().getModel("PurchaseLiteModel").setProperty("/entry",oNew);
+			var that = this;
+			if (this.ImageDialog === undefined) {
+				this.ImageDialog = new Dialog({
+					title: "Choose JPG/JPEG File For Upload",
+					beginButton: new sap.m.Button({
+						text: "Close",
+						press: function(oEvent) {
+							that.ImageDialog.close();
+						}
+					}),
+					content: [
+						new FileUploader("PhotoMassUploader", {
+							fileType: "jpeg,jpg",
+							change: [this.onMassImageUpload, this],
+							
+							multiple:true
+						})
+					]
+				});
+				this.getView().addDependent(this.ImageDialog);
+			}
+			this.ImageDialog.open();
+		},
+		onMassImageUpload: function(oEvent) {
+			debugger;
+			var photo=[];
+			this.photoCount=0;
 			var that=this;
-			if(!this.oAddProduct){
-			Fragment.load({
+			const files = oEvent.getParameter("files");
+			for (let i = 0; i < files.length; i++) {
+				//const img = document.createElement("img");
+				var reader = new FileReader();
+				reader.onload = function(e) {
+					debugger;
+					try {
+						var vContent = e.currentTarget.result; 
+						var _allImages=that.getView().getModel("PurchaseLiteModel").getProperty("/PurchaseLite");
+						var mPhoto=that.getView().getModel("PurchaseLiteModel").getProperty("/MassPhoto");
+						for (var i = 0; i < _allImages.length; i++) {
+							debugger;
+							if(_allImages[i].TagNo.toUpperCase()===mPhoto[that.photoCount].Name.split(".")[0].toUpperCase()){
+								mPhoto[that.photoCount].Content=vContent;
+								_allImages[i].PhotoCheck=true;
+								_allImages[i].Photo.push(mPhoto[that.photoCount]);
+								that.photoCount=that.photoCount+1;
+								that.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite",_allImages);
+								that.ImageDialog.close();
+								that.getView().byId("PurchaseLiteTable").getBinding("rows").refresh();
+								break;
+							}
+						}
+					} catch (e) {
+
+					}
+				}
+				var img = {
+					"Name": "",
+					"Stream": "",
+					"Content": ""
+				};
+				// debugger;
+				img.Name = files[i].name;
+				img.Stream = URL.createObjectURL(files[i]);
+				reader.readAsDataURL(files[i]);
+				photo.push(img);
+				// debugger;
+			}
+			this.getView().getModel("PurchaseLiteModel").setProperty("/MassPhoto",photo);
+		},
+		onAdd: function() {
+			debugger;
+			var oNew = {
+				ItemCode: "***",
+				TagNo: "",
+				GWt: "",
+				Amount: "",
+				PCS: "",
+				Size: "",
+				Remark: "",
+				Photo: [],
+				PhotoCheck: false
+			};
+			this.getView().getModel("PurchaseLiteModel").setProperty("/entry", oNew);
+			var that = this;
+			if (!this.oAddProduct) {
+				Fragment.load({
 					id: "idAddProduct",
 					name: "sap.ui.demo.cart.fragments.PurchaseStyleCURD",
 					controller: this
 				}).then(function(oPopup) {
 					that.oAddProduct = oPopup;
 					that.getView().addDependent(oPopup);
-					oPopup.setTitle("ADD");	
+					oPopup.setTitle("ADD");
 					oPopup.open();
 				});
-			}
-			else{
+			} else {
 				this.oAddProduct.open();
 			}
 		},
-		onEdit:function(oEvent){
+		onEdit: function(oEvent) {
 			debugger;
-			var that=this;
-			var sPath=oEvent.getSource().getParent().getParent().getRowBindingContext().getPath();
-			var value=this.getView().getModel("PurchaseLiteModel").getProperty(sPath);
-			this.getView().getModel("PurchaseLiteModel").setProperty("/entry",value);
-			if(!this.oEditProduct){
-			Fragment.load({
+			var that = this;
+			this.EditPath = oEvent.getSource().getParent().getParent().getRowBindingContext().getPath();
+			var value = JSON.parse(JSON.stringify(this.getView().getModel("PurchaseLiteModel").getProperty(this.EditPath)));
+			this.getView().getModel("PurchaseLiteModel").setProperty("/entry", value);
+			if (!this.oEditProduct) {
+				Fragment.load({
 					id: "idEditProduct",
 					name: "sap.ui.demo.cart.fragments.PurchaseStyleCURD",
 					controller: this
@@ -146,22 +217,21 @@ sap.ui.define([
 					that.oEditProduct = oPopup;
 					that.getView().addDependent(oPopup);
 					oPopup.setTitle("EDIT");
-					oPopup.getButtons()[0].setText("Edit");	
+					oPopup.getButtons()[0].setText("Edit");
 					oPopup.open();
 				});
-			}
-			else{
+			} else {
 				this.oEditProduct.open();
 			}
 		},
-		onCopy:function(oEvent){
+		onCopy: function(oEvent) {
 			debugger;
-			var that=this;
-			var sPath=oEvent.getSource().getParent().getParent().getRowBindingContext().getPath();
-			var value=this.getView().getModel("PurchaseLiteModel").getProperty(sPath);
-			this.getView().getModel("PurchaseLiteModel").setProperty("/entry",value);
-			if(!this.oCopyProduct){
-			Fragment.load({
+			var that = this;
+			var sPath = oEvent.getSource().getParent().getParent().getRowBindingContext().getPath();
+			var value = JSON.parse(JSON.stringify(this.getView().getModel("PurchaseLiteModel").getProperty(sPath)));
+			this.getView().getModel("PurchaseLiteModel").setProperty("/entry", value);
+			if (!this.oCopyProduct) {
+				Fragment.load({
 					id: "idCopyProduct",
 					name: "sap.ui.demo.cart.fragments.PurchaseStyleCURD",
 					controller: this
@@ -169,71 +239,74 @@ sap.ui.define([
 					that.oCopyProduct = oPopup;
 					that.getView().addDependent(oPopup);
 					oPopup.setTitle("COPY");
-					oPopup.getButtons()[0].setText("Copy");	
+					oPopup.getButtons()[0].setText("Copy");
 					oPopup.open();
 				});
-			}
-			else{
+			} else {
 				this.oCopyProduct.open();
 			}
 		},
-		onPressHandleSecureCancelPopup:function(oEvent){
+		onPressHandleSecureCancelPopup: function(oEvent) {
 			debugger;
 			oEvent.getSource().getParent().close();
-			// this.oAddProduct.close();
+			MessageToast.show("Operation Cancelled");
 		},
-		onPressHandleSecureOkPopup:function(oEvent){
-			var sId=oEvent.getSource().getParent().getId().split("--")[0];
-			if(sId==="idAddProduct"){
+		onPressHandleSecureOkPopup: function(oEvent) {
+			var sId = oEvent.getSource().getParent().getId().split("--")[0];
+			var value=this.getView().getModel("PurchaseLiteModel").getProperty("/entry");
+			var data=this.getView().getModel("PurchaseLiteModel").getProperty("/PurchaseLite");
+			if (sId === "idAddProduct") {
+				data.push(value);
 				debugger;
-				MessageToast.show("Under Construction...");
+				this.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite",data);
+				MessageToast.show("Data Added Successful");
 
-			}
-			else if(sId==="idEditProduct"){
+			} else if (sId === "idEditProduct") {
 				debugger;
-				MessageToast.show("Under Construction...");
-			}
-			else if(sId==="idCopyProduct"){
+				this.getView().getModel("PurchaseLiteModel").setProperty(this.EditPath,value);
+				MessageToast.show("Data Edited Successful");
+			} else if (sId === "idCopyProduct") {
+				data.push(value);
+				this.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite",data);
 				debugger;
-				MessageToast.show("Under Construction...");
+				MessageToast.show("Data Copied Successful");
 			}
-			// this.oAddProduct.close();
 			oEvent.getSource().getParent().close();
 		},
-		onPressTemplateDownload:function(){
+		onPressTemplateDownload: function() {
 			debugger;
 			var aCols, aMessages, oSettings;
 			// if (Array.isArray(exportData) && exportData.length) {
-				aCols = this._createExcelColumns();
-				var reportExcel = [];
-				for(var i=1;i<51;i++){
-					reportExcel.push({
-						ItemCode:"***",
-						TagNo: "T-"+i,
-						GWt: i*10,
-						Amount: (i*10)+(i*5),
-						PCS: i,
-						Size: i,
-						Remark: i,
-						Photo:[],
-						PhotoCheck:false
-					});
-				}
-				// reportExcel.push("TEMPLATE");
-				oSettings = {
-					workbook: {
-						columns: aCols
-					},
-					dataSource: reportExcel
-				};
-				new Spreadsheet(oSettings)
-					.build()
-					.then(function() {
-						MessageToast.show("Spreadsheet export has finished");
-					});
+			aCols = this._createExcelColumns();
+			var reportExcel = [];
+			for (var i = 1; i < 51; i++) {
+				reportExcel.push({
+					ItemCode: "***",
+					TagNo: "T-" + i,
+					GWt: i * 10,
+					Amount: (i * 10) + (i * 5),
+					PCS: i,
+					Size: i,
+					Remark: i,
+					Photo: [],
+					PhotoCheck: false
+				});
+			}
+			// reportExcel.push("TEMPLATE");
+			oSettings = {
+				workbook: {
+					columns: aCols
+				},
+				dataSource: reportExcel
+			};
+			new Spreadsheet(oSettings)
+				.build()
+				.then(function() {
+					MessageToast.show("Spreadsheet export has finished");
+				});
 			// }
 		},
-		_createExcelColumns:function(){
+		_createExcelColumns: function() {
 			return [
 				// {
 				// 	label: 'Action',
@@ -263,35 +336,34 @@ sap.ui.define([
 				}
 			];
 		},
-		onPhotoClick:function(oEvent){
+		onPhotoClick: function(oEvent) {
 			debugger;
-			var that=this;
-			this.PhotoPath=oEvent.getSource().getParent().getRowBindingContext().getPath();
-			var Images=this.getView().getModel("PurchaseLiteModel").getProperty(this.PhotoPath+"/Photo");
-			this.getView().getModel("PurchaseLiteModel").setProperty("/allImages",Images);
-			this.PhotoButton=oEvent.getSource();
+			var that = this;
+			this.PhotoPath = oEvent.getSource().getParent().getRowBindingContext().getPath();
+			var Images = JSON.parse(JSON.stringify(this.getView().getModel("PurchaseLiteModel").getProperty(this.PhotoPath + "/Photo")));
+			this.getView().getModel("PurchaseLiteModel").setProperty("/allImages", Images);
+			// this.PhotoButton = oEvent.getSource();
 
-			if(!this.oPhotoPopUp){
+			if (!this.oPhotoPopUp) {
 				Fragment.load({
-						id: "idPhotoPopUp",
-						name: "sap.ui.demo.cart.fragments.PurchasePhoto",
-						controller: this
-					}).then(function(oPopup) {
-						that.oPhotoPopUp = oPopup;
-						debugger;
-						that.getView().addDependent(oPopup);
-						oPopup.setTitle("ADD");	
-						// sap.ui.getCore().byId("idPhotoPopUp--gridList").bindAggregation(that.getView().getModel("PurchaseLiteModel").getProperty(spath+"/Photo"));
-						oPopup.open();
+					id: "idPhotoPopUp",
+					name: "sap.ui.demo.cart.fragments.PurchasePhoto",
+					controller: this
+				}).then(function(oPopup) {
+					that.oPhotoPopUp = oPopup;
+					debugger;
+					that.getView().addDependent(oPopup);
+					oPopup.setTitle("ADD");
+					// sap.ui.getCore().byId("idPhotoPopUp--gridList").bindAggregation(that.getView().getModel("PurchaseLiteModel").getProperty(spath+"/Photo"));
+					oPopup.open();
 				});
-			}
-			else{
+			} else {
 				debugger;
 				// sap.ui.getCore().byId("idPhotoPopUp--gridList").bindItems();
 				this.oPhotoPopUp.open();
 			}
 		},
-		onUploadChange:function(oEvent){
+		onUploadChange: function(oEvent) {
 			debugger;
 			const files = oEvent.getParameter("files");
 			var that = this;
@@ -302,19 +374,18 @@ sap.ui.define([
 				for (let i = 0; i < files.length; i++) {
 					//const img = document.createElement("img");
 					var reader = new FileReader();
-					reader.onload = function(e){
+					reader.onload = function(e) {
 						var _allImages = that.getView().getModel("PurchaseLiteModel").getProperty("/allImages");
 						// var _allImages=[];
 						debugger;
 						try {
-							var vContent = e.currentTarget.result; //.result.replace("data:image/jpeg;base64,", "");
+							var vContent = e.currentTarget.result;
 							for (var i = 0; i < _allImages.length; i++) {
-								if(!_allImages[i].Content){
-									_allImages[i].Content = vContent;//that.getView().getModel("PurchaseLiteModel").setProperty("/PurchaseLite",excelD);
+								if (!_allImages[i].Content) {
+									_allImages[i].Content = vContent; 
 									that.getView().getModel("PurchaseLiteModel").setProperty("/allImages", _allImages);
-									that.getView().getModel("PurchaseLiteModel").setProperty(this.PhotoPath+"/Photo", _allImages);
-									that.getView().getModel("PurchaseLiteModel").setProperty(this.PhotoPath+"/PhotoCheck", true);
-									// that.getView().byId("PurchaseLiteTable").
+									debugger;
+									that.getView().byId("idPhotoPopUp--gridList").getBinding("items").refresh();
 									break;
 								}
 							}
@@ -323,12 +394,12 @@ sap.ui.define([
 						}
 					};
 					var img = {
-						"Name":"",
+						"Name": "",
 						"Stream": "",
 						"Content": ""
 					};
 					debugger;
-					img.Name=files[i].name;
+					img.Name = files[i].name;
 					img.Stream = URL.createObjectURL(files[i]);
 					reader.readAsDataURL(files[i]);
 					allImages.push(img);
@@ -336,12 +407,18 @@ sap.ui.define([
 				}
 			}
 		},
-		//oEvent.getSource().getParent().close();
-		onOkayPhotoFrag:function(oEvent){
+		onOkayPhotoFrag: function(oEvent) {
 			debugger;
-			// this.PhotoButton.setType("Accept");
-			this.byId("PurchaseLiteTable").getBinding("rows").refresh();
+			var allImages = this.getView().getModel("PurchaseLiteModel").getProperty("/allImages");
+			this.getView().getModel("PurchaseLiteModel").setProperty(this.PhotoPath + "/PhotoCheck",true);
+			this.getView().getModel("PurchaseLiteModel").setProperty(this.PhotoPath + "/Photo",allImages);
+			this.getView().byId("PurchaseLiteTable").getBinding("rows").refresh();
 			oEvent.getSource().getParent().close();
+			MessageToast.show("Photo added Successfully");
+		},
+		onCancelPhotoFrag:function(oEvent){
+			oEvent.getSource().getParent().close();
+			MessageToast.show("Operation Cancelled");
 		}
 	});
 });
