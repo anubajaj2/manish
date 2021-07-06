b8fcd80f09c2033bcdb472e52641a28d007f3c74
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-07-06 16:12:20 +0530

    Blocks

--- src/webapp/sharedBlock/PurchaseLeingthBlock.js
+++ src/webapp/sharedBlock/PurchaseLeingthBlock.js
@@ -6,14 +6,14 @@ sap.ui.define(["sap/ui/core/library", 'sap/uxap/BlockBase'], function(coreLibrar
 	var PurchaseLeingthBlock = BlockBase.extend("studentportal.sharedBlock.PurchaseLeingthBlock", {
 		metadata: {
 			views: {
-				Collapsed: {
-					viewName: "sap.ui.demo.cart.view.PurchaseLeingth",
-					type: ViewType.XML
-				},
-				Expanded: {
-					viewName: "sap.ui.demo.cart.view.PurchaseLeingth",
-					type: ViewType.XML
-				}
+				// Collapsed: {
+				// 	viewName: "sap.ui.demo.cart.view.PurchaseLeingth",
+				// 	type: ViewType.XML
+				// },
+				// Expanded: {
+				// 	viewName: "sap.ui.demo.cart.view.PurchaseLeingth",
+				// 	type: ViewType.XML
+				// }
 			}
 		}
 	});
