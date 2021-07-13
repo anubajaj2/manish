ff54bfa1e3169150672d10ff3e1357db35625851
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-07-13 15:21:17 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -398,7 +398,7 @@ app.get('/getpattern',
       })
       .then(function(Products) {
         debugger;
-        if(Products.length===0)
+        if(Products.length===0){
           res.send("1");
         }
         var pCount = Products[0]["Count"];
