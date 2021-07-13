6df4c5a1959cd1acd9ed095b451f028aaad67819
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-07-13 16:01:20 +0530

    maker count

--- server/server.js
+++ server/server.js
@@ -401,7 +401,7 @@ app.get('/getpattern',
       .then(function(Products) {
         debugger;
         if(Products.length===0){
-          res.send("1");
+          res.send("0");
         }
         var pCount = Products[0]["Count"];
         res.send(pCount.toString());
