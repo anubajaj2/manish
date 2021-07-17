cb9a22e343351fdb396305c8640d47e71a3415cd
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-07-17 12:16:30 +0530

    view

--- src/webapp/sharedBlock/PurchaseDetailBlock.js
+++ src/webapp/sharedBlock/PurchaseDetailBlock.js
@@ -6,14 +6,14 @@ sap.ui.define(["sap/ui/core/library", 'sap/uxap/BlockBase'], function(coreLibrar
 	var PurchaseDetailBlock = BlockBase.extend("studentportal.sharedBlock.PurchaseDetailBlock", {
 		metadata: {
 			views: {
-				// Collapsed: {
-				// 	viewName: "sap.ui.demo.cart.view.PurchaseDetail",
-				// 	type: ViewType.XML
-				// },
-				// Expanded: {
-				// 	viewName: "sap.ui.demo.cart.view.PurchaseDetail",
-				// 	type: ViewType.XML
-				// }
+				Collapsed: {
+					viewName: "sap.ui.demo.cart.view.PurchaseDetail",
+					type: ViewType.XML
+				},
+				Expanded: {
+					viewName: "sap.ui.demo.cart.view.PurchaseDetail",
+					type: ViewType.XML
+				}
 			}
 		}
 	});
