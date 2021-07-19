828003d55c8ec9bf190bcb9fc858e5965bcc02f9
kuldeep singh <kuldeepsingh@soyuztechnologies.com>
2021-07-19 20:17:10 +0530

    Order Items update

--- src/webapp/controller/MyItemsApproval.controller.js
+++ src/webapp/controller/MyItemsApproval.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
 	"sap/ui/demo/cart/controller/BaseController",
 	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel"
-], function (BaseController, UIComponent, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (BaseController, UIComponent, JSONModel,History,Filter,FilterOperator) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.cart.controller.MyItemsApproval", {
@@ -23,6 +26,9 @@ sap.ui.define([
 			// var cUser = this.getView().getModel("local").getProperty("/CurrentUser");;
 			// var oFilter = new sap.ui.model.Filter("ApproverId", sap.ui.model.FilterOperator.EQ, cUser);
 			// this.getView().byId("idOrderItemTable").getBinding("items").filter(oFilter);
+			if (this.getOwnerComponent().getModel('local').getProperty('/Authorization') === "") {
+				this.logOutApp();
+			}
 			var CreatedBy = this.getView().getModel("local").getProperty("/CurrentUser");
 			var that = this;
 			$.get("/OrderItemApproval?Createdby=" + CreatedBy)
@@ -45,7 +51,7 @@ sap.ui.define([
 			})
 				.done(function (data, status) {
 					debugger;
-					
+
 				})
 				.fail(function (xhr, status, error) {
 					// sap.ui.core.BusyIndicator.hide();
@@ -53,9 +59,42 @@ sap.ui.define([
 					debugger;
 				});
 		},
-		onFilterSelect: function () {
+		onFilterSelect: function (oEvent) {
 			debugger;
-			sap.m.MessageToast("Under Process");
-		}
+			var oBinding = this.getView().byId("idOrderItemTable").getBinding("items");
+				var sKey = oEvent.getParameter("key");
+				var aFilters = [];
+				var oFilter1;
+				var oFilter2;
+
+			if (sKey === "All") {
+				// oFilter1 = new Filter("OrderStatus", sap.ui.model.FilterOperator.EQ, "");
+				oFilter1 = [];
+
+
+			} else if (sKey === "New") {
+				oFilter1 = new Filter("Status", sap.ui.model.FilterOperator.EQ, 'N');
+
+
+			} else if (sKey === "Approved") {
+				oFilter1 = new Filter("Status", sap.ui.model.FilterOperator.EQ, "A");
+			} else if (sKey === "Rejected") {
+				oFilter1 = new Filter("Status", sap.ui.model.FilterOperator.EQ, "R");
+			}
+
+			aFilters.push(oFilter1);
+			oBinding.filter(aFilters);
+
+		},
+		onNavBack: function() {
+			debugger;
+			var oHistory = History.getInstance();
+			var oPrevHash = oHistory.getPreviousHash();
+			if (oPrevHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("Profile");
+			}
+		},
 	});
 });
