fa8dc5988a107f5a973d0c16b2e36d2ec9eac1af
anubajaj2 <anurag.bajaj02@gmail.com>
2021-07-22 10:38:49 +0530

    hook method

--- common/models/orderItem.js
+++ common/models/orderItem.js
@@ -3,4 +3,16 @@
 
 module.exports = function(OrderItem) {
 
+  // OrderItem.observe('after save', async function(ctx){
+  //     var app = require('../../server/server');
+  //     var Inquiry = app.models.Inquiry;
+  //     var idProd = ctx.instance.__data.ProductId.toString();
+  //     if(ctx.isNewInstance === false){
+  // 			next();
+  // 		}
+  //     Product.findOne({
+  //
+  //     })
+  // });
+
 };
